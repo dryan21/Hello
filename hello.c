a7f10741a1420fcca6f39399e5960a3010ca7c80
dryan21 <54606645+dryan21@users.noreply.github.com>
2019-08-29 11:04:35 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Dillon Ryan and partner
+ * Date: 2019/08/27
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Dillon Ryan\n");
+
+  return 0;
+}

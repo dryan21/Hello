95441e5b24a1c743bd2ca2faff9593b2bf4af4d9
jpreister4 <54559118+jpreister4@users.noreply.github.com>
2019-08-29 11:30:03 -0500

    Added 2 Name/Major
    Add my own name and major

--- hello.c
+++ hello.c
@@ -12,6 +12,8 @@ int main(int argc, char **argv) {
 
   printf("Dillon Ryan\n");
   printf("Electrical Engineering\n");
+  printf("%sJoshua Preister\n", );
+  printf("%sEconomics and Computer Science\n", );
 
   return 0;
 }
